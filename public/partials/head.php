b7b47e881e49bff4759f70ffab692f861b5a8178
TechZeeLand <techzeeland@gmail.com>
2026-09-10 20:12:34 +0600

    metadata updated

--- public/partials/head.php
+++ public/partials/head.php
@@ -19,13 +19,14 @@ $pageDescription = $pageDescription ?? 'Free, open-source assignment cover page 
 <meta property="og:type" content="website" />
 <meta property="og:title" content="<?php echo htmlspecialchars($pageTitle, ENT_QUOTES); ?>" />
 <meta property="og:description" content="<?php echo htmlspecialchars($pageDescription, ENT_QUOTES); ?>" />
-<meta property="og:image" content="assets/img/cover.png" />
+<meta property="og:image" content="assets/img/cover.jpg" />
+<meta property="og:image:alt" content="Cover Image" />
 
 <!-- Twitter Card -->
 <meta name="twitter:card" content="summary_large_image">
 <meta name="twitter:title" content="<?php echo htmlspecialchars($pageTitle, ENT_QUOTES); ?>">
 <meta name="twitter:description" content="<?php echo htmlspecialchars($pageDescription, ENT_QUOTES); ?>">
-<meta name="twitter:image" content="assets/img/cover.png">
+<meta name="twitter:image" content="assets/img/cover.jpg">
 
 <!-- Favicons -->
 <link rel="icon" type="image/svg+xml" href="assets/img/logo.svg" />
